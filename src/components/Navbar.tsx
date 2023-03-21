ec2ce70bbbfe25bc3b8286f89b4126b29d145eb0
parixscm <parixscm@gmail.com>
2023-03-21 12:25:28 +0900

    📝 Feat: ADD navbar with style

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { BiShoppingBag } from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 z-20 w-full">
+      <div className="container mx-auto flex items-center justify-between px-2 py-4">
+        <span className="text-xl font-bold">Shopify</span>
+        <div className="relative cursor-pointer">
+          <BiShoppingBag className="text-3xl opacity-80" />
+          <div className="absolute right-[-3px] bottom-[-3px] z-10 flex h-4 w-4 items-center justify-center rounded-full bg-black text-[10px] text-white">
+            0
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
